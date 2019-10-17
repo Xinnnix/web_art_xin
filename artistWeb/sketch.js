a82ed0b7a0e3177406e88042d2b26cac6629aa85
Xinnnix <xzhang19@saic.edu>
2019-10-16 20:44:52 -0500

    add new files

---
+++ artistWeb/sketch.js
@@ -0,0 +1,12 @@
+// function setup() {
+//     createCanvas(windowWidth,windowHeight);
+//     background(random(255),random(255),random(255))
+//     frameRate(60);
+//   }
+  
+//   function draw() {
+//      fill(random(255),random(255),random(255));
+//      for(let i = 0; i < 200; i+=20){
+//       ellipse(width/2, height/2, sin(frameCount/i)*3*mouseX, mouseY);
+//     }
+//   }
---
+++ session13/Gallery/session13.js
@@ -0,0 +1,31 @@
+let container = document.getElementById('container');
+
+
+
+// container.addEventListener('click', function(){
+//     console.log("clicked!");
+
+//     let popout = document.getElementById("popout");
+//     popout.style.display = 'none';
+//     if (popout.style.display === 'none'){
+//         popout.style.display = 'block';
+    
+//     }
+//     else{
+//         popout.style.display = 'none';
+//     }
+// }); 
+
+let items = container.getElementsByClassName("gallery-item");
+let popout = document.getElementById("popout");
+for (let i = 0; i < items.length; i++){
+    items[i].addEventListener('click', function(){
+        if (popout.style.display === 'block'){
+            popout.style.display = 'none';
+        
+        }
+        else{
+            popout.style.display = 'block';
+        }
+    })
+}
