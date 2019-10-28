8bd5361648bcd59ece7c066e5e3f52cf0108c862
xinnnix <zxgilbert@gmail.com>
2019-10-28 10:05:56 -0500

    first p5 sketch

---
+++ firstP5Sketch/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  let cnv = createCanvas(640, 480);
+  cnv.position(100,100);
+  background(255,10,80);
+}
+
+function draw() {
+
+  circle(100,50,100);
+  circle(200,50,100);
+  circle(150,100,100);
+  circle(100,150,100);
+  circle(200,150,100);
+  square(50,50,100);
+  rect(150,150,100,200);
+  rect(250,350,300,100);
+  rect(150,350,100,100);
+  circle(400,175,300);
+  square(350,125,100);
+  rect(75,250,50,200);
+}
